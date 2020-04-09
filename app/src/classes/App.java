6b6db6e45804f3aab88ea6981edb11a2e34da62b
Andrey <andrey.richman@gmail.com>
2020-04-09 20:00:20 +0300

    First commit

--- app/src/classes/App.java
+++ app/src/classes/App.java
@@ -6,7 +6,7 @@ public class App {
     UIHandler uiHandler;
     public App() {
         uiHandler = new UI();
-        uiHandler.ShowOutput("App created!");
+        uiHandler.ShowOutput("App created successfully!");
     }
     public void run(){
         uiHandler.ShowOutput("App is Running!");

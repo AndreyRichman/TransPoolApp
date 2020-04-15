6b4fc8fad2d4e8a4d49267af0e3e62f5f670ba20
Matan Kochavi <kochavimatan@gmail.com>
2020-04-15 17:48:54 +0300

    Road: created ctr created get & set for 3 attributes

--- logic/src/classes/Road.java
+++ logic/src/classes/Road.java
@@ -8,8 +8,33 @@ public class Road {
     private int fuelCost;
     private int maxSpeed;
 
-    public Road(){
+    public Road(Station startStation, Station endStation){
+        this.startStation = startStation;
+        this.endStation = endStation;
+    }
 
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setFuelCost(int fuelCost) {
+        this.fuelCost = fuelCost;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getFuelCost() {
+        return fuelCost;
     }
 
     public Station getStartStation() {
